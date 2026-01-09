8a512d7b0e3e625d8bf565c7c79f1d9742e8a964
Growing in faith Global Church <growinginfaithglobalchurch@gmail.com>
2026-01-09 07:05:02 +0000

    Try fixing this error: `Runtime Error: useSidebar must be used within a

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,6 @@
 import { AppHeader } from '@/components/app-header';
 import { AppSidebar } from '@/components/app-sidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function AppLayout({
   children,
@@ -7,14 +8,16 @@ export default function AppLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[280px_1fr]">
-      <AppSidebar />
-      <div className="flex flex-col">
-        <AppHeader />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background">
-          {children}
-        </main>
+    <SidebarProvider>
+      <div className="grid min-h-screen w-full md:grid-cols-[280px_1fr]">
+        <AppSidebar />
+        <div className="flex flex-col">
+          <AppHeader />
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background">
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
