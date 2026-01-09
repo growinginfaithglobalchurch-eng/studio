acf63f9b451e277b496f4adf75b2191507da4750
Growing in faith Global Church <growinginfaithglobalchurch@gmail.com>
2026-01-09 08:05:11 +0000

    The headers font should be Anton font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['"Poppins"', 'sans-serif'],
-        headline: ['"Montserrat"', 'serif'],
+        headline: ['"Anton"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
