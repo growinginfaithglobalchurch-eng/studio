8c7cbe2839279b6cb1a2c445a7041fba10c2ee0a
Growing in faith Global Church <growinginfaithglobalchurch@gmail.com>
2026-01-09 11:48:36 +0000

    Try fixing this error: `Runtime ReferenceError: React is not defined. Er

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 
 
+import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { feedItems, communityUsers } from "@/lib/data";
