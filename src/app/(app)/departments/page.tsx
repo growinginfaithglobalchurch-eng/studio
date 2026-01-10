1d8abd61ddefb98d050022d54b935c621ddc0dc5
Growing in faith Global Church <growinginfaithglobalchurch@gmail.com>
2026-01-10 05:03:26 +0000

    On departments the Join button texts color make it white

--- src/app/(app)/departments/page.tsx
+++ src/app/(app)/departments/page.tsx
@@ -62,7 +62,7 @@ export default function DepartmentsPage() {
                         View
                     </Link>
                 </Button>
-                <Button className="w-full" variant="outline">
+                <Button className="w-full text-white" variant="outline">
                     <UserPlus className="mr-2 h-4 w-4" />
                     Join 
                 </Button>
