5eb085357eb79dd5ccd764afac0a88f69b427490
Growing in faith Global Church <growinginfaithglobalchurch@gmail.com>
2026-01-11 00:19:50 +0000

    Try fixing this error: `Runtime ReferenceError: React is not defined. Er

--- src/app/(app)/mercy-city/page.tsx
+++ src/app/(app)/mercy-city/page.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import React from 'react';
 import { useState } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
