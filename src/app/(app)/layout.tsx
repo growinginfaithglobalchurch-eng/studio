f45f6514bd9610ee50754506a26879a864168d2f
Growing in faith Global Church <growinginfaithglobalchurch@gmail.com>
2026-01-11 21:17:26 +0000

    The top Header of the app should not move app and down when scrolling on

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,6 +2,7 @@
 import { AppHeader } from '@/components/app-header';
 import { AppBottomNav } from '@/components/app-bottom-nav';
 import { AppSidebar } from '@/components/app-sidebar';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 export default function AppLayout({
   children,
@@ -9,13 +10,15 @@ export default function AppLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen w-full">
+    <div className="flex h-screen w-full">
       <AppSidebar />
-      <div className="flex flex-1 flex-col">
+      <div className="flex flex-1 flex-col overflow-hidden">
         <AppHeader />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background pb-20 md:pb-6">
-          {children}
-        </main>
+        <ScrollArea className="flex-1">
+          <main className="flex-1 gap-4 p-4 lg:gap-6 lg:p-6 bg-background pb-20 md:pb-6">
+            {children}
+          </main>
+        </ScrollArea>
         <AppBottomNav />
       </div>
     </div>
